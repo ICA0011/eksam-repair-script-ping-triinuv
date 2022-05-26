176c327b19b7d3cc7d9cd641fff89ecd74edadae
Triinu-Liis Vaikma <92437926+triinuv@users.noreply.github.com>
2022-05-26 12:30:06 +0300

    Update ping.py

--- ping.py
+++ ping.py
@@ -1,8 +1,9 @@
 import requests
 
-def check_server_status():
-    a = requests.get(url)
-    if a = 200:
+def check_server_status(url):
+    request_response = requests.head(url)
+    a = request_response.status_code
+    if a == 200:
         return True
     else:
         return False
